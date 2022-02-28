9dcfa560a74629e0ce84041fa3b538698a90784f
giovanny <giovanny@hq.bereg.net>
2022-02-28 14:34:26 +0300

    cоздание логики

--- internal/ggraph/ggraph.go
+++ internal/ggraph/ggraph.go
@@ -3,9 +3,11 @@ package ggraph
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/Giovanny472/ggraph/model"
 	"github.com/goccy/go-graphviz"
+	"github.com/goccy/go-graphviz/cgraph"
 )
 
 type ggraph struct {
@@ -27,16 +29,16 @@ func (gr *ggraph) SetSimpleAdjMatrix(adm *model.AdjMatrix) {
 func (gr *ggraph) SetDirectedAdjMatrix(adm *model.AdjMatrix) {
 
 	// создание графа
-	gviz := graphviz.New()
-	graph01, err := gviz.Graph()
+	agviz := graphviz.New()
+	aggraph, err := agviz.Graph()
 	if err != nil {
-		log.Println("graph01 err: " + err.Error())
+		log.Println("aggraph err: " + err.Error())
 	}
 	defer func() {
-		if err := graph01.Close(); err != nil {
+		if err := aggraph.Close(); err != nil {
 			log.Fatal(err)
 		}
-		gviz.Close()
+		agviz.Close()
 	}()
 
 	// количество вершин
@@ -47,6 +49,76 @@ func (gr *ggraph) SetDirectedAdjMatrix(adm *model.AdjMatrix) {
 	aCountEdge := len((*adm)[0])
 	fmt.Println("size_edge --> ", aCountEdge)
 
+	// cоздание набора вершин
+	var listNodes []*cgraph.Node
+	for idx := 0; idx < aCountNodes; idx++ {
+
+		anode, _ := aggraph.CreateNode("N" + strconv.Itoa(idx+1))
+		listNodes = append(listNodes, anode)
+	}
+
+	// определение набора ребер
+	var alistEdges model.AdjListEdge
+
+	for idxCol := 0; idxCol < aCountEdge; idxCol++ {
+
+		// ребер
+		var aEdge *model.AdjEdge
+
+		// cоздание
+		aEdge = new(model.AdjEdge)
+
+		for idxRow := 0; idxRow < aCountNodes; idxRow++ {
+
+			// получаем значение
+			aval := (*adm)[idxRow][idxCol]
+			fmt.Println("aval --> ", aval)
+
+			// проверка
+			if aval == 0 {
+				continue
+			}
+
+			if aval == -1 || aval == 1 {
+
+				// название ребра
+				aEdge.NodeName = "e" + strconv.Itoa(idxCol+1)
+
+				// start
+				if aval == -1 {
+					aEdge.NodeStart = listNodes[idxRow]
+				}
+
+				// end
+				if aval == 1 {
+					aEdge.NodeEnd = listNodes[idxRow]
+				}
+
+				fmt.Println("aEdge: ", aEdge)
+			}
+
+		}
+
+		if aEdge != nil {
+			if aEdge.NodeName != "" &&
+				aEdge.NodeStart != nil &&
+				aEdge.NodeEnd != nil {
+
+				alistEdges = append(alistEdges, *aEdge)
+			}
+		}
+		aEdge = nil
+		fmt.Println(alistEdges)
+		fmt.Println("--------------")
+	}
+
+	for _, anode := range alistEdges {
+
+		aed, _ := aggraph.CreateEdge(anode.NodeName, anode.NodeStart, anode.NodeEnd)
+		aed.SetLabel(anode.NodeName)
+	}
+	// создание
+	agviz.RenderFilename(aggraph, graphviz.PNG, "graph.png")
 }
 
 func (gr *ggraph) Save() {
--- model/ggraph.go
+++ model/ggraph.go
@@ -1,7 +1,17 @@
 package model
 
+import "github.com/goccy/go-graphviz/cgraph"
+
 type AdjRow []int
 type AdjMatrix []AdjRow
+
+type AdjEdge struct {
+	NodeName  string
+	NodeStart *cgraph.Node
+	NodeEnd   *cgraph.Node
+}
+
+type AdjListEdge []AdjEdge
 
 type GGraph interface {
 	SetSimpleAdjMatrix(adm *AdjMatrix)
